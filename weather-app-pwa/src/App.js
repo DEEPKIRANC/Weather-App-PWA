ca642cceb963ccc1a633ebf43fc501f66ff4938a
Deep Kiran <cdeepkiran14@gmail.com>
2021-05-18 16:37:08 +0530

    Fetch Weather Data from API call

---
+++ weather-app-pwa/src/App.js
@@ -0,0 +1,26 @@
+import React,{useState} from "react";
+import {fetchWeather} from "./api/fetchWeather";
+import "./App.css"
+const App=()=>{
+    const [query,setQuery]=useState('');
+
+    const search=async(e)=>{
+        if(e.key==="Enter")
+        {
+            const data=await fetchWeather(query);
+            console.log(data);
+        }
+    }
+    return <div className="main-container">
+        <input 
+        type="text"
+        className="search"
+        placeholder="Enter City..."
+        value={query}
+        onChange={e=>setQuery(e.target.value)}
+        onKeyPress={search}
+         />
+    </div>
+}
+
+export default App
